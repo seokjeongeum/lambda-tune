79f5197dc1e11df4753da62377d1f38f93b25dbb
Jeongeum Seok <jeseok@dblab.postech.ac.kr>
2025-03-25 00:27:01 +0000

    Replace e9 input job script with TPCH benchmark script; enhance configuration and execution parameters

--- e9_input_job_tpch.sh
+++ e9_input_job_tpch.sh
@@ -9,6 +9,35 @@ rm -r ./lambdatune/configs/e9
 rm -r ./test/e9
     
 .venv/bin/python lambdatune/run_lambdatune.py \
+    --benchmark tpch\
+    --system POSTGRES \
+    --configs ./lambdatune/configs/e9/tpch/lambdatune \
+    --out ./test/e9/tpch/lambdatune \
+    --config_gen config_gen \
+    --core 64 \
+    --memory 128 \
+    --token_budget 786\
+    --exploit_index exploit_index\
+    --order_query order_query\
+    --query_weight query_weight\
+    
+    
+.venv/bin/python lambdatune/run_lambdatune.py \
+    --benchmark tpch\
+    --system POSTGRES \
+    --configs ./lambdatune/configs/e9/tpch/ours \
+    --out ./test/e9/tpch/ours \
+    --config_gen config_gen \
+    --core 64 \
+    --memory 128 \
+    --token_budget 786\
+    --exploit_index exploit_index\
+    --order_query order_query\
+    --query_weight query_weight\
+    --workload_statistics workload_statistics\
+
+    
+.venv/bin/python lambdatune/run_lambdatune.py \
     --benchmark job\
     --system POSTGRES \
     --configs ./lambdatune/configs/e9/job/lambdatune \
